dbfbb2b0a6b3ecc8bbf1aa83de1474e9135133ea
drewteachout <Zflicka1>
2019-01-16 14:35:05 -0500

    Created tab component. Added tabs to the header

--- a1-rentals/src/app/app.module.ts
+++ a1-rentals/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { TabsComponent } from './header/tabs/tabs.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    HomepageComponent
+    HomepageComponent,
+    TabsComponent
   ],
   imports: [
     BrowserModule

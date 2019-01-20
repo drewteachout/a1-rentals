327c7fb13b9a4b7fb074149985335a8f4f90b1c0
Conor Brownell <cbrownell24@gmail.com>
2019-01-20 18:43:58 -0500

    implementing a form, step 1

--- a1-rentals/src/app/app.module.ts
+++ a1-rentals/src/app/app.module.ts
@@ -1,20 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TabsComponent } from './header/tabs/tabs.component';
+import { FormComponent } from './form/form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomepageComponent,
-    TabsComponent
+    TabsComponent,
+    FormComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ a1-rentals/src/app/form/form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './contact';
+
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+
+export class FormComponent implements OnInit {
+
+  //model = new Contact(18, 'Conor', 'Brownell', 'cbrownell3@gatech.edu', 'Test', 'Test message');
+
+  submitted = false;
+
+  onSubmit() {this.submitted = true; }
+
+  // TODO: Remove this when we're done
+  // get diagnostic() { return JSON.stringify(this.model); }
+  
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

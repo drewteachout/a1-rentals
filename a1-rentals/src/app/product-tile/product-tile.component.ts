42f7a83e854b7329fa14e91ffcb5393762a4466d
drewteachout <Zflicka1>
2019-01-22 18:46:09 -0500

    product-tile prgress

--- a1-rentals/src/app/product-tile/product-tile.component.ts
+++ a1-rentals/src/app/product-tile/product-tile.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit, Input } from '@angular/core';
 export class ProductTileComponent implements OnInit {
 
   @Input() productName: string;
+  @Input() productSource: string;
 
   constructor() { }
 

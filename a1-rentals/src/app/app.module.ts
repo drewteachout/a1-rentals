69b64af81711cef202d1d225ee3a6622b54cb127
drewteachout <Zflicka1>
2019-01-22 14:31:39 -0500

    Created product tile component

--- a1-rentals/src/app/app.module.ts
+++ a1-rentals/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomepageComponent } from './homepage/homepage.component';
 import { TabsComponent } from './header/tabs/tabs.component';
+import { ProductTileComponent } from './product-tile/product-tile.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     HomepageComponent,
-    TabsComponent
+    TabsComponent,
+    ProductTileComponent
   ],
   imports: [
     BrowserModule
---
+++ a1-rentals/src/app/product-tile/product-tile.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-tile',
+  templateUrl: './product-tile.component.html',
+  styleUrls: ['./product-tile.component.css']
+})
+export class ProductTileComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}

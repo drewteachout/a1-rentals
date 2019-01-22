662cae06be39e1fc3a31990222a2b8ba25749a3e
drewteachout <Zflicka1>
2019-01-22 14:28:11 -0500

    Tabs properly dynamic space subTabs

--- a1-rentals/src/app/header/tabs/tabs.component.ts
+++ a1-rentals/src/app/header/tabs/tabs.component.ts
@@ -15,8 +15,8 @@ export class TabsComponent implements OnInit {
 
   constructor() {
     this.tab1 = ['Popular Products', []];
-    this.tab2 = ['Rental Products', [['Chairs', ['Big Chair', 'Little Chair', 'Our Chair']], ['Tents', []],
-    ['Lights', []]]];
+    this.tab2 = ['Rental Products', [['Chairs', []], ['Tents', ['Elite Pole Tents', 'Frame Tents']],
+      ['Lights', ['L.E.D. Dance Floor Lights', 'Lighted Tables', 'LED Furniture Rentals', 'Uplighting Rentals']]]];
     this.tab3 = ['Packages', []];
     this.tab4 = ['Contact Us', []];
     this.tabs = [this.tab1, this.tab2, this.tab3, this.tab4];
@@ -25,4 +25,12 @@ export class TabsComponent implements OnInit {
   ngOnInit() {
   }
 
+  // Calculates appropriate padding necessary based on where we are in the list
+  setSubTabStyle(index, list) {
+    let percentage = (100/list.length * index).toString() + '%';
+    let styles = {
+      'top': percentage
+    };
+    return styles;
+  }
 }

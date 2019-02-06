4e8952e5c627c7596c7a254280d2716d76be0e41
William Syre <wsyre3@gatech.edu>
2019-02-06 00:07:09 -0500

    Quote Cart nearly complete

--- a1-rentals/src/app/app.module.ts
+++ a1-rentals/src/app/app.module.ts
@@ -49,7 +49,7 @@ const appRoutes: Routes = [
   imports: [
     RouterModule.forRoot(
       appRoutes,
-      { enableTracing: true } // <-- debugging purposes only
+      //{ enableTracing: true } // <-- debugging purposes only
     ),
     BrowserModule,
     AgGridModule.withComponents([]),
--- a1-rentals/src/app/quote-cart/quote-cart.component.ts
+++ a1-rentals/src/app/quote-cart/quote-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Cart_Item } from '../util/Cart_Item';
+import { QuoteCartServiceService } from '../quote-cart-service.service';
 
 @Component({
   selector: 'app-quote-cart',
@@ -7,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteCartComponent implements OnInit {
 
-  constructor() { }
+  cartService: QuoteCartServiceService
+  cart: Cart_Item[] = [new Cart_Item("Elite Pole Tent 1", 5, "The most elite pole tent money can buy buy buy buy buy buy buy buy buy", 100),
+  new Cart_Item("Elite Pole Tent 2", 5, "The most elite pole tent money can buy", 100),
+  new Cart_Item("Elite Pole Tent 3", 5, "The most elite pole tent money can buy", 100),
+  new Cart_Item("Elite Pole Tent 4", 5, "The most elite pole tent money can buy", 100)];
+  quote: String = "qoute";
 
-  ngOnInit() {
+  constructor(cartService: QuoteCartServiceService) {
+    this.cartService = cartService;
+    this.cartService.update(this.cart);
+    this.cartService.get().subscribe((newCart: Cart_Item[]) => {
+      this.cart = [];
+      for(let i = 0; i < newCart.length; i++) {
+        this.cart.push(newCart[i]);
+      }
+    });
+  }
+
+  ngOnInit() {  
+    console.log(this.cart);
+  }
+
+  deleteTile(cartItem: Cart_Item) {
+    let newCart: Cart_Item[] = [];
+    this.cart = this.cart.filter((a) => a.productName != cartItem.productName);
+    this.cartService.update(this.cart);
+    console.log(cartItem.productName);
   }
 
 }

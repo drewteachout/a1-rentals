1fdaeb0f3ac1d2b3bd23a03d334d570a5991ce0e
Drew Teachout <drewteachout@gmail.com>
2019-02-07 15:23:59 -0500

    Fixed style errors

--- a1-rentals/src/app/quote-cart/quote-cart.component.ts
+++ a1-rentals/src/app/quote-cart/quote-cart.component.ts
@@ -9,34 +9,34 @@ import { QuoteCartServiceService } from '../services/quote-cart-service.service'
 })
 export class QuoteCartComponent implements OnInit, AfterViewInit{
 
-  cartService: QuoteCartServiceService
+  cartService: QuoteCartServiceService;
   cart: Cart_Item[] = [];
-  quote: String = "qoute";
+  quote: String = 'qoute';
 
   constructor(cartService: QuoteCartServiceService) {
     this.cartService = cartService;
     this.cartService.get().subscribe((newCart: Cart_Item[]) => {
       this.cart = [];
-      for(let i = 0; i < newCart.length; i++) {
+      for (let i = 0; i < newCart.length; i++) {
         this.cart.push(newCart[i]);
       }
     });
   }
 
-  ngOnInit() {  
+  ngOnInit() {
     console.log(this.cart);
   }
 
   ngAfterViewInit() {
-    document.getElementById("Popular Products").className = "button-tab primary";
-    document.getElementById("Rental Products").className = "button-tab primary";
-    document.getElementById("Packages").className = "button-tab primary";
-    document.getElementById("Contact Us").className = "button-tab primary";
+    document.getElementById('Popular Products').className = 'button-tab primary';
+    document.getElementById('Rental Products').className = 'button-tab primary';
+    document.getElementById('Packages').className = 'button-tab primary';
+    document.getElementById('Contact Us').className = 'button-tab primary';
   }
 
   deleteTile(cartItem: Cart_Item) {
-    let newCart: Cart_Item[] = [];
-    this.cart = this.cart.filter((a) => a.productName != cartItem.productName);
+    const newCart: Cart_Item[] = [];
+    this.cart = this.cart.filter((a) => a.productName !== cartItem.productName);
     this.cartService.update(this.cart);
     console.log(cartItem.productName);
   }

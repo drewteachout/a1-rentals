0f67e5066870fbc6051838155a0fbf02b1bb2530
William Syre <wsyre3@gatech.edu>
2019-02-20 14:23:54 -0500

    removed admin tab

--- a1-rentals/src/app/header/tabs/tabs.component.ts
+++ a1-rentals/src/app/header/tabs/tabs.component.ts
@@ -21,39 +21,36 @@ export class TabsComponent implements OnInit {
   constructor(private prodServ: ProductsService, private router: Router, private db: AngularFirestore) {
     this.tab1 = ['Popular Products', []];
     this.tab2 = ['Rental Products', []];
-    
     this.db.collection('/products').valueChanges().subscribe((productNames: any[]) => {
       productNames.forEach(product => {
-        if(!product['hidden']) {
+        if (!product['hidden']) {
           this.db.collection('/' + product['collection_name']).valueChanges().subscribe((productInfo: any) => {
-            console.log("Updating " + productInfo)
-            let nextProductList: any[] = [product['display_name'], []]
-            for(let i = 0; i < productInfo.length; i++) {
-              if(productInfo[i].hasOwnProperty('name')) {
-                if(!productInfo[i]['hidden']) {
+            console.log('Updating ' + productInfo)
+            const nextProductList: any[] = [product['display_name'], []]
+            for (let i = 0; i < productInfo.length; i++) {
+              if (productInfo[i].hasOwnProperty('name')) {
+                if (!productInfo[i]['hidden']) {
                   nextProductList[1].push(productInfo[i]['name']);
                 }
               }
             }
-            let flag = false
-            for(let i = 0; i < this.tab2[1].length; i++) {
-              if(this.tab2[1][i][0] == nextProductList[0]) {
-                this.tab2[1][i] = nextProductList
-                flag = true
+            let flag = false;
+            for (let i = 0; i < this.tab2[1].length; i++) {
+              if(this.tab2[1][i][0] === nextProductList[0]) {
+                this.tab2[1][i] = nextProductList;
+                flag = true;
               }
             }
-            if(!flag) {
+            if (!flag) {
               this.tab2[1].push(nextProductList);
             }
-            
           });
         }
       });
     });
     this.tab3 = ['Packages', []];
     this.tab4 = ['Contact Us', []];
-    this.tab5 = ['Admin Tools', []];
-    this.tabs = [this.tab1, this.tab2, this.tab3, this.tab4, this.tab5];
+    this.tabs = [this.tab1, this.tab2, this.tab3, this.tab4];
   }
 
   ngOnInit() {

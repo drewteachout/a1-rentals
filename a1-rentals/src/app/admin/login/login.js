e3805a7992652229b78884290613231d50a65cc2
Sam Loop <samloop16@gmail.com>
2019-03-08 13:52:14 -0500

    apparently useless code

--- a1-rentals/src/app/admin/login/login.js
+++ a1-rentals/src/app/admin/login/login.js
@@ -1,186 +1,48 @@
-/*
- * Copyright 2016 Google Inc. All Rights Reserved.
- *
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
- * in compliance with the License. You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software distributed under the
- * License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
- * express or implied. See the License for the specific language governing permissions and
- * limitations under the License.
- */
+import { promises } from "fs";
 
-/**
- * FirebaseUI initialization to be used in a Single Page application context.
- */
+(function () {
 
-/**
- * @return {!Object} The FirebaseUI config.
- */
-function getUiConfig() {
-    return {
-      'callbacks': {
-        // Called when the user has been successfully signed in.
-        'signInSuccessWithAuthResult': function(authResult, redirectUrl) {
-          if (authResult.user) {
-            handleSignedInUser(authResult.user);
-          }
-          if (authResult.additionalUserInfo) {
-            document.getElementById('is-new-user').textContent =
-                authResult.additionalUserInfo.isNewUser ?
-                'New User' : 'Existing User';
-          }
-          // Do not redirect.
-          return false;
+// initialize Firebase
+    const config = {
+        apiKey: "AIzaSyBRPFTZdKuMG508q5nHu7BRQjPa69y8nwA",
+        authDomain: "a1-rentals.firebaseapp.com",
+        databaseURL: "https://a1-rentals.firebaseio.com",
+        projectId: "a1-rentals"
+    };
+    firebase.initializeApp(config);
+
+//get elements
+    const txtEmail = document.getElementById('txtEmail');
+    const txtPassword = document.getElementById('txtPassword');
+    const btnLogin = document.getElementById('btnLogin');
+    const btnLogout = document.getElementById('btnLogout');
+
+//add login event
+    btnLogin.addEventListener('click', e => {
+        //get email and password
+        // TODO: check for real email
+        const email = txtEmail.value;
+        const pass = txtPassword.value;
+        const auth = firebase.auth();
+        //sign in
+        auth.signInWithEmailAndPassword(email, pass);
+        promises.catch( e => console.log(e.message));
+    });
+
+    btnLogout.addEventListener('click', e=> {
+        firebase.auth().signOut();
+    });
+
+    //add realtime listener
+    firebase.auth().onAuthStateChanged(firebaseUser => {
+        if(firebaseUser) {
+            console.log(firebaseUser);
+            btnLogout.classList.remove('hide');
+        } else {
+            console.log('Not logged in.');
+            btnLogout.classList.add('hide');
         }
-      },
-      // Opens IDP Providers sign-in flow in a popup.
-      'signInFlow': 'popup',
-      'signInOptions': [
-        // TODO(developer): Remove the providers you don't need for your app.
-        // {
-        //   provider: firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-        //   // Required to enable this provider in One-Tap Sign-up.
-        //   authMethod: 'https://accounts.google.com',
-        //   // Required to enable ID token credentials for this provider.
-        //   clientId: CLIENT_ID
-        // },
-        // {
-        //   provider: firebase.auth.FacebookAuthProvider.PROVIDER_ID,
-        //   scopes :[
-        //     'public_profile',
-        //     'email',
-        //     'user_likes',
-        //     'user_friends'
-        //   ]
-        // },
-        // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
-        // firebase.auth.GithubAuthProvider.PROVIDER_ID,
-        {
-          provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
-          // Whether the display name should be displayed in Sign Up page.
-          requireDisplayName: true,
-          signInMethod: getEmailSignInMethod()
-        },
-        // {
-        //   provider: firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-        //   recaptchaParameters: {
-        //     size: getRecaptchaMode()
-        //   }
-        // },
-      ]
-    //   // Terms of service url.
-    //   'tosUrl': 'https://www.google.com',
-    //   // Privacy policy url.
-    //   'privacyPolicyUrl': 'https://www.google.com',
-    //   'credentialHelper': CLIENT_ID && CLIENT_ID != 'YOUR_OAUTH_CLIENT_ID' ?
-    //       firebaseui.auth.CredentialHelper.GOOGLE_YOLO :
-    //       firebaseui.auth.CredentialHelper.ACCOUNT_CHOOSER_COM
-     };
-  }
-  
-  // Initialize the FirebaseUI Widget using Firebase.
-  var ui = new firebaseui.auth.AuthUI(firebase.auth());
-  // Disable auto-sign in.
-  ui.disableAutoSignIn();
-  
-  
-  /**
-   * @return {string} The URL of the FirebaseUI standalone widget.
-   */
-  function getWidgetUrl() {
-    return '/widget#recaptcha=' + getRecaptchaMode() + '&emailSignInMethod=' +
-        getEmailSignInMethod();
-  }
-  
-  
-  /**
-   * Redirects to the FirebaseUI widget.
-   */
-  var signInWithRedirect = function() {
-    window.location.assign(getWidgetUrl());
-  };
-  
-  
-  /**
-   * Open a popup with the FirebaseUI widget.
-   */
-  var signInWithPopup = function() {
-    window.open(getWidgetUrl(), 'Sign In', 'width=985,height=735');
-  };
-  
-  
-  /**
-   * Displays the UI for a signed in user.
-   * @param {!firebase.User} user
-   */
-  var handleSignedInUser = function(user) {
-    document.getElementById('user-signed-in').style.display = 'block';
-    document.getElementById('user-signed-out').style.display = 'none';
-    document.getElementById('name').textContent = user.displayName;
-    document.getElementById('email').textContent = user.email;
-  };
-  
-  
-  /**
-   * Displays the UI for a signed out user.
-   */
-  var handleSignedOutUser = function() {
-    document.getElementById('user-signed-in').style.display = 'none';
-    document.getElementById('user-signed-out').style.display = 'block';
-    ui.start('#firebaseui-container', getUiConfig());
-  };
-  
-  // Listen to change in auth state so it displays the correct UI for when
-  // the user is signed in or not.
-  firebase.auth().onAuthStateChanged(function(user) {
-    document.getElementById('loading').style.display = 'none';
-    document.getElementById('loaded').style.display = 'block';
-    user ? handleSignedInUser(user) : handleSignedOutUser();
-  });
-  
-  /**
-   * Deletes the user's account.
-   */
-  var deleteAccount = function() {
-    firebase.auth().currentUser.delete().catch(function(error) {
-      if (error.code == 'auth/requires-recent-login') {
-        // The user's credential is too old. She needs to sign in again.
-        firebase.auth().signOut().then(function() {
-          // The timeout allows the message to be displayed after the UI has
-          // changed to the signed out state.
-          setTimeout(function() {
-            alert('Please sign in again to delete your account.');
-          }, 1);
-        });
-      }
     });
-  };
-  
-  /**
-   * Initializes the app.
-   */
-  var initApp = function() {
-    document.getElementById('sign-in-with-redirect').addEventListener(
-        'click', signInWithRedirect);
-    document.getElementById('sign-in-with-popup').addEventListener(
-        'click', signInWithPopup);
-    document.getElementById('sign-out').addEventListener('click', function() {
-      firebase.auth().signOut();
-    });
-    document.getElementById('delete-account').addEventListener(
-        'click', function() {
-          deleteAccount();
-        });
-  
-  
-    document.getElementById('email-signInMethod-password').addEventListener(
-        'change', handleConfigChange);
-    document.querySelector(
-        'input[name="emailSignInMethod"][value="' + getEmailSignInMethod() + '"]')
-        .checked = true;
-  };
-  
-  window.addEventListener('load', initApp);
+
+
+});

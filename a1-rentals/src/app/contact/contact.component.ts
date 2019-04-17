10d713c5124919729292cd1a30058d9747f8cf96
William Syre <wsyre3@gatech.edu>
2019-04-16 21:59:47 -0400

    contact UI now reflects database

--- a1-rentals/src/app/contact/contact.component.ts
+++ a1-rentals/src/app/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AngularFirestore } from 'angularfire2/firestore';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-contact',
@@ -7,7 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactComponent implements OnInit {
 
-  constructor() { }
+  address = '';
+  businessHours = '';
+  phone = '';
+  constructor(private db: AngularFirestore) {
+    this.db.collection('Contact').valueChanges().pipe((map((docs: any[]) => docs[0]))).subscribe((doc: any) => {
+      this.address = doc['Address'];
+      this.businessHours = doc['Business Hours'];
+      this.phone = doc['Phone'];
+    });
+  }
 
   ngOnInit() {
   }
